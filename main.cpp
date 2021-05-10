5d72ff89d3e276bf63bf80e5b6ca775d84abaccb
Y.N。 <monhun0904@icloud.com>
2021-05-10 12:32:17 +0900

    ヘッダー追加

--- main.cpp
+++ main.cpp
@@ -13,9 +13,9 @@
 //Windowsのプログラミング方法 = (WinAPI)で動いている！
 //DxLibは、DirectXという、ゲームプログラミングをかんたんに使える仕組み
 int WINAPI WinMain(
-	HINSTANCE hInstance, 
-	HINSTANCE hPrevInstance, 
-	LPSTR lpCmdLine, 
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
 	int nCmdShow)
 {
 	SetOutApplicationLogValidFlag(FALSE);
@@ -31,68 +31,30 @@ int WINAPI WinMain(
 
 
 	//ＤＸライブラリ初期化処理
-	if (DxLib_Init() == -1)	
+	if (DxLib_Init() == -1)
 	{
 		// エラーが起きたら直ちに終了
 		return -1;
 	}
 
-	int x = GAME_WIDTH / 2;
-	int y = GAME_HEIGHT / 2;
-
-	int width = 32;
-	int height = 32;
-
-	int radius = 100;
-
-	int Speed = 1;
-	int XSpeed = Speed;
-	int YSpeed = Speed;
+	
 
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	//無限ループ
 	while (1)
 	{
-		//何かしらキーが押されたとき
-		if (CheckHitKeyAll() != 0)	//0のときに、何かキーが押された
-		{
-			break;	//無限ループを抜ける
-		}
+		
 
 		//メッセージを受け取り続ける
-		if (ProcessMessage() != 0)	//-1のとき、エラーやウィンドウが閉じられた
-		{
-			break;	//無限ループを抜ける
-		}
+		if (ProcessMessage() != 0) { break; }
 
 		if (ClearDrawScreen() != 0) { break; }
 
-		//DrawBox(x, y, x + width, y + height, GetColor(255, 0, 0), TRUE);
-		DrawCircle(x, y, radius, GetColor(0, 255, 0), FALSE, 5);
-		x += XSpeed;
-		y += YSpeed;
-
-		if (x - radius < 0 || x + radius > GAME_WIDTH) {
-			XSpeed = -XSpeed;
-
-			if (XSpeed > 0) { XSpeed += 1; }
-			else if (XSpeed < 0) { XSpeed -= 1; }
-		}
-
-		if (y - radius < 0 || y + radius > GAME_HEIGHT) {
-			YSpeed = -YSpeed;
-		}
-
-		if (YSpeed > 0) { YSpeed += 1; }
-		else if (YSpeed < 0) { YSpeed -= 1; }
-
 		ScreenFlip();
+		//ＤＸライブラリ使用の終了処理
+		DxLib_End();
+
+		return 0;	// ソフトの終了 
 	}
-
-
-	//ＤＸライブラリ使用の終了処理
-	DxLib_End();
-
-	return 0;	// ソフトの終了 
 }
